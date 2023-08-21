72285b32f3ed908f526cef53933ec5878cc10d7b
smillyPOP <smillypop@pop-os.localdomain>
2023-08-21 00:38:57 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,56 @@ import {formatTimestamp} from './util/convertTimestamp'
 import { APPtimeStamp, APPvalue, } from './util/APP';
 import { DLRValue, DLRtimeStamp, } from './util/dlr';
 import { SLRSlice } from './util/SLRSlice';
+import MySlider from './components/myslider';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [startTimestamp, setStartTimestamp] = useState(1660759200000);
+  const [endTimestamp, setEndTimestamp] = useState(1691772300000);
+  const [dateRange, setDateRange] = useState([startTimestamp, endTimestamp]);
+
+
+  function handleSliderChange(range) {
+    const monthToTimestamp = (monthsSinceStart) => {
+      const date = new Date(startTimestamp);
+      date.setMonth(date.getMonth() + monthsSinceStart);
+      return date.getTime();
+    };
+  
+    const startDate = monthToTimestamp(range[0]);
+    const endDate = monthToTimestamp(range[1]);
+  
+    setDateRange([startDate, endDate]);
+  }
 
   const SLRTimeStamp = SLRSlice(DLRtimeStamp)
 
   const combinedTimestamps = [...APPtimeStamp, ...DLRtimeStamp];
   
   const allTimestamps = Array.from(new Set(combinedTimestamps)).sort((a, b) => a - b);
+  const filteredTimestamps = allTimestamps.filter((ts => ts >= dateRange[0] && ts <= dateRange[1]));
 
-  const SLRData = allTimestamps.map(ts => {
+
+  const SLRData = filteredTimestamps.map(ts => {
     const index = SLRTimeStamp.indexOf(ts);
     return index !== -1 ? 1250 : null;
   });
 
-  const APPData = allTimestamps.map(ts => {
+  const APPData = filteredTimestamps.map(ts => {
     const index = APPtimeStamp.indexOf(ts);
     return index !== -1 ? APPvalue[index] : null;
   });
   
-  const DLRData = allTimestamps.map(ts => {
+  const DLRData = filteredTimestamps.map(ts => {
     const index = DLRtimeStamp.indexOf(ts);
     return index !== -1 ? DLRValue[index] : null;
   });
   
 
+
+  console.log(filteredTimestamps)
 
 
   const colors = ['#5470C6', '#EE6666', "#c54fd6"];
@@ -56,7 +81,7 @@ function App() {
     },
     xAxis: {
       type: 'category',
-      data: allTimestamps.map(ts => formatTimestamp(ts))
+      data: filteredTimestamps.map(ts => formatTimestamp(ts))
     },
     yAxis: [
       {
@@ -108,6 +133,9 @@ function App() {
   return (
     <div style={{height: '100vh', width: '100%'}}>
       <h1>Meu App com ECharts</h1>
+      {startTimestamp && endTimestamp && (
+    <MySlider onSliderChange={handleSliderChange} startTimestamp={startTimestamp} endTimestamp={endTimestamp} />
+)}
       <ReactEcharts option={chartOptions} style={{ height: '400px', width: '100%' }}/>
     </div>
   )
